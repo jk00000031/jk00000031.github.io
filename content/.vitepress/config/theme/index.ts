9da4dc8c89a831d1241a8246688bfdd58958f291
黄俊康 <849989428@qq.com>
2024-05-22 14:58:42 +0800

    feat(footer): update

--- content/.vitepress/config/theme/index.ts
+++ content/.vitepress/config/theme/index.ts
@@ -16,7 +16,7 @@ export const themeConfig: DefaultTheme.Config = {
     linkText: "前往首页",
   },
   footer: {
-    message: `本站内的所有内容均可进行转发，但请注明出处。如果在阅读过程中发现内容有误，欢迎将内容链接和截图发送到 <a href="email:849989428@qq.com">849989428@qq.com</a>。`,
+    message: `本站内的所有内容均可进行转发，但请注明出处。<br />如果在阅读过程中发现内容有误，欢迎将内容链接和截图发送到 <a href="mailto:849989428@qq.com">849989428@qq.com</a>。`,
     copyright: `Copyright © 2024-present <a href="https://github.com/jk00000031" target="__blank">jk00000031</a>`,
   },
   socialLinks: [
