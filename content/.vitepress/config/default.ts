2391541e20c1b8a3e398dcacfe813ef7a7a45fec
黄俊康 <849989428@qq.com>
2024-05-22 11:42:16 +0800

    feat(config): favicon

--- content/.vitepress/config/default.ts
+++ content/.vitepress/config/default.ts
@@ -4,6 +4,7 @@ export const defaultConfig: Omit<UserConfig<DefaultTheme.Config>, "themeConfig">
   title: "拖延者服务",
   description: "虽然站内有各种想到的创意点，但如标题说的那样，我是个拖延症患者。",
   lang: "zh-CN",
+  head: [["link", { rel: "icon", href: "/favicon.ico" }]],
   locales: {
     root: {
       label: "简体中文",
